9b7f7f2bf8c23f2dbdc8102cc42f45b43a6f4401
Fabrício ADM <fabriciobaraunadev@gmail.com>
2023-12-15 00:08:44 -0300

    evento de clique, mudança de imagem

--- Fokus-projeto-base/main.js
+++ Fokus-projeto-base/main.js
@@ -4,17 +4,23 @@ const html = document.querySelector('html') //seleciona a tag html
 const focusBt = document.querySelector('.app__card-button--foco')
 const curtoBt = document.querySelector('.app__card-button--curto')
 const longoBt = document.querySelector('.app__card-button--longo')
+const banner = document.querySelector('.app__image')
 
 
 focusBt.addEventListener('click', ()=> {
     html.setAttribute('data-contexto', 'foco')
+    banner.setAttribute('src', '/Fokus-projeto-base/imagens/foco.png')
 })
 
 curtoBt.addEventListener('click', ()=> {
     html.setAttribute('data-contexto', 'descanso-curto')
+    banner.setAttribute('src', '/Fokus-projeto-base/imagens/descanso-curto.png')
+
 })
 
 longoBt.addEventListener('click', ()=> {
     html.setAttribute('data-contexto', 'descanso-longo')
+    banner.setAttribute('src', '/Fokus-projeto-base/imagens/descanso-longo.png')
+
 })
 
