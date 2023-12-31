71185b3ae7a1ab745639b2460b3daa7134236eb0
Fabrício ADM <fabriciobaraunadev@gmail.com>
2023-12-31 00:04:21 -0300

    modificando titulo com innerHTML

--- Fokus-projeto-base/main.js
+++ Fokus-projeto-base/main.js
@@ -5,6 +5,7 @@ const focoBt = document.querySelector('.app__card-button--foco')
 const curtoBt = document.querySelector('.app__card-button--curto')
 const longoBt = document.querySelector('.app__card-button--longo')
 const banner = document.querySelector('.app__image')
+const titulo = document.querySelector('.app__title')
 
 
 focoBt.addEventListener('click', () => {
@@ -20,8 +21,22 @@ longoBt.addEventListener('click', ()=> {
     alterarContexto('descanso-longo')
 })
 
-function alterarContexto(contexto){
-    html.setAttribute('data-contexto', contexto)
-    banner.setAttribute('src', `/Fokus-projeto-base/imagens/${contexto}.png`)  
-} 
+function alterarContexto(contexto) {
+    html.setAttribute('data-contexto', contexto);
+    banner.setAttribute('src', `/Fokus-projeto-base/imagens/${contexto}.png`);
+
+    switch (contexto) {
+        case "foco":
+            titulo.innerHTML = `Otimize sua produtividade,<br><strong class="app__title-strong">mergulhe no que importa.</strong>`;
+            break;
+        case "descanso-curto":
+            titulo.innerHTML = `Que tal dar uma respirada <br><strong class="app__title-strong"> Faça uma pausa curta </strong>`;
+            break;
+        case "descanso-longo":
+            titulo.innerHTML = `Hora de voltar à superfície<br><strong class="app__title-strong"> Faça uma pausa longa </strong>`;
+            break;
+        default:
+            break;
+    }
+}
 
